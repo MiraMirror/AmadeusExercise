5911a15bc27dc23635dada08b563d1ea89f0f079
MiraMirror <schwinn.young@gmail.com>
2015-04-06 14:46:38 -0400

    add new updates
    1. finish Exercise 1,2,3
    2. update code to Python 3 & Pandas 0.16
    3. Django webserver setup

--- DataMiningExercise.py
+++ DataMiningExercise.py
@@ -5,35 +5,37 @@ import datetime
 import matplotlib.pyplot as plt
 import pylab
 
+##bookings = pd.DataFrame(pd.read_csv('bookings.csv', error_bad_lines = False, delimiter = '^'))
+##searches = pd.DataFrame(pd.read_csv('searches.csv', error_bad_lines = False, delimiter = '^'))
 
-bookings = pd.DataFrame(pd.read_csv('bookings.csv', error_bad_lines = False, delimiter = '^'))
-searches = pd.DataFrame(pd.read_csv('searches.csv', error_bad_lines = False, delimiter = '^'))
+bookings = pd.DataFrame(pd.read_csv('bookings_sample.csv'))
+searches = pd.DataFrame(pd.read_csv('searches_sample.csv'))
 
-bookings.dtypes
-bookings.describe()
-
-# strip out spaces in the column names
+##bookings.dtypes
+##bookings.describe()
 bookings.columns = [x.replace(' ','') for x in list(bookings.columns)]
 searches.columns = [x.replace(' ','') for x in list(searches.columns)]
 
 
 
-### First exercise ###
-
-print "The number of lines in bookings is " + str(bookings.shape[0])
-print "The number of lines in searches is " + str(searches.shape[0])
-
-# Results:
-# The number of lines in bookings is 10000010
-# The number of lines in searches is 20390198
+##### First exercise ###
+##
+##print("The number of lines in bookings is " + str(bookings.shape[0]))
+##print("The number of lines in searches is " + str(searches.shape[0]))
+##
+### Results:
+### The number of lines in bookings is 10000010
+### The number of lines in searches is 20390198
 
 
 ### Second exercise ###
 # Top 10 arrival airports in 2013 by number of passengers
+
 bookings2013 = bookings[bookings['year'] == 2013]
 Arrport_Pax_2013 = bookings2013[['arr_port','pax']].groupby(['arr_port']).sum()
+Arrport_Pax_2013 = Arrport_Pax_2013.sort(columns = 'pax', ascending = False)
 Top10_Arrport = Arrport_Pax_2013.sort(columns = 'pax', ascending = False).head(10)
-print Top10_Arrport
+print(Top10_Arrport)
 
 #  Results:
 #              pax
@@ -49,27 +51,30 @@ print Top10_Arrport
 #  SFO       58000
 #  DXB       55590
 
-### Third exercise ###
+##### Third exercise ###
+##
+####searches.dtypes
+####searches.describe()
+##
+### select searches for flights arriving at Malaga, Madrid, or Barcelona
+##MMB_airports = list(['AGP',
+##                     'MAD',
+##                     'BCN'])
+##
+##searches_MMB = searches[searches['Destination'].isin(MMB_airports)][['Date','Destination']]
+##searches_MMB['Month'] = np.nan
+##searches_MMB['Month'] = searches_MMB['Date'].apply(lambda x: datetime.datetime.strftime(datetime.datetime.strptime(x, '%Y-%m-%d'), '%Y-%m'))
+##searches_MMB_pivot = pd.pivot_table(searches_MMB[['Month','Destination']], index='Month', columns='Destination', aggfunc = np.size)
+##searches_MMB_pivot.columns = ['Malaga', 'Madrid', 'Barcelona']
+##
+### Plot
+##plt.figure()
+##plt_searches = searches_MMB_pivot.plot(title = 'Searches by Month')
+##plt_searches.set_ylabel('Total Searches')
+##pylab.show()
 
-searches.dtypes
-searches.describe()
 
-# select searches for flights arriving at Malaga, Madrid, or Barcelona
-MMB_airports = list(['AGP',
-                     'MAD',
-                     'BCN'])
-
-# need date in month
-searches_MMB = searches[searches['Destination'].isin(MMB_airports)][['Date','Destination']]
-
-searches_MMB_pivot = pd.pivot_table(searches_MMB, rows='Date', cols='Destination', aggfunc = np.size)
-
-# need new legend
-plt.figure()
-searches_MMB_pivot.plot()
-pylab.show()
-
-
+### Bonus exercise 1 ###
 
 
 
